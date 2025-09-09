48a6329e079d6827354184b7af0e6be88c4e8f9e
abdulfalaq5 <abdulfalaq5@gmail.com>
2025-09-09 11:19:04 +0700

    Fix: issue statis

--- src/main.ts
+++ src/main.ts
@@ -85,7 +85,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  // await app.listen(process.env.PORT ?? 3000);
-  await app.listen(9526);
+  await app.listen(process.env.PORT ?? 3000);
+  // await app.listen(9526);
 }
 void bootstrap();
