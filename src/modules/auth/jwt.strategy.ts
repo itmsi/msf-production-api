ad8fdfca2daa2bf087af7cb8dfb7eacf54153a39
panji <panjiforlak@gmail.com>
2025-09-20 03:16:33 +0700

    Simpan perubahan lokal sementara

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: configService.get('JWT_SECRET'),
+      secretOrKey: configService.get('JWT_SECRET') || 'defaultIfEmpty',
       ignoreExpiration: false,
     });
   }
